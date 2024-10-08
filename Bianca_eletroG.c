6e859519427b90e5a90e60bb49e75a8322c58a0e
BiancaRebouca <biancareboucasoliveira@gmail.com>
2024-10-08 09:47:57 -0300

    Add files via upload

---
+++ Bianca_eletroG.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(){
+    int idadeBianca = 17;
+    int idadeLucas = 17;
+
+    if(idadeBianca == idadeLucas){
+        printf("Bianca tem a mesma idade que Lucas!");
+    }
+    else { printf("Lucas nao tem a mesma idade Bianca!");
+    }
+
+    }
+    
